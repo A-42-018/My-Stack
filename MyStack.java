1d8bcfa9885e723c6542b240ba447122856a102e
alifnew8531 <139584880+alifnew8531@users.noreply.github.com>
2026-04-07 21:07:09 +0600

    Feature: Convert stack to generic type T

--- MyStack.java
+++ MyStack.java
@@ -1,18 +1,20 @@
 import java.util.ArrayList;
 
-public class MyStack {
-    private ArrayList<Integer> elements = new ArrayList<>();
+public class MyStack<T> {
+    private ArrayList<T> elements = new ArrayList<>();
 
-    public int top() {
+    public T top() {
         return elements.get(elements.size() - 1);
     }
 
-    public void push(int n) {
-        elements.add(n);
+    public void push(T item) {
+        elements.add(item);
     }
 
     public void pop() {
-        elements.remove(elements.size() - 1);
+        if (!isEmpty()) {
+            elements.remove(elements.size() - 1);
+        }
     }
 
     public boolean isEmpty() {
